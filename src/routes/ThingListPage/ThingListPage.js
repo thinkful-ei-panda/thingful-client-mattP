20393721d67d2d9659838afe5a08ea3ed74bb959
Matt <63121708+themlp101@users.noreply.github.com>
2020-07-22 17:28:01 -0400

    using local server with auth

--- src/routes/ThingListPage/ThingListPage.js
+++ src/routes/ThingListPage/ThingListPage.js
@@ -6,33 +6,34 @@ import ThingListItem from '../../components/ThingListItem/ThingListItem'
 import './ThingListPage.css'
 
 export default class ThingListPage extends Component {
-  static contextType = ThingListContext
+	static contextType = ThingListContext
 
-  componentDidMount() {
-    this.context.clearError()
-    ThingApiService.getThings()
-      .then(this.context.setThingList)
-      .catch(this.context.setError)
-  }
+	componentDidMount() {
+		this.context.clearError()
+		ThingApiService.getThings()
+			.then(this.context.setThingList)
+			.catch(this.context.setError)
+	}
 
-  renderThings() {
-    const { thingList = [] } = this.context
-    return thingList.map(thing =>
-      <ThingListItem
-        key={thing.id}
-        thing={thing}
-      />
-    )
-  }
+	renderThings() {
+		const { thingList = [] } = this.context
+		return thingList.map((thing) => (
+			<ThingListItem key={thing.id} thing={thing} />
+		))
+	}
 
-  render() {
-    const { error } = this.context
-    return (
-      <Section list className='ThingListPage'>
-        {error
-          ? <p className='red'>There was an error, try again</p>
-          : this.renderThings()}
-      </Section>
-    )
-  }
+	render() {
+		const { error } = this.context
+		return (
+			<Section list className='ThingListPage'>
+				{error ? (
+					<p className='red'>
+						There was an error, try again
+					</p>
+				) : (
+					this.renderThings()
+				)}
+			</Section>
+		)
+	}
 }
--- src/routes/ThingPage/ThingPage.js
+++ src/routes/ThingPage/ThingPage.js
@@ -8,86 +8,86 @@ import ReviewForm from '../../components/ReviewForm/ReviewForm'
 import './ThingPage.css'
 
 export default class ThingPage extends Component {
-  static defaultProps = {
-    match: { params: {} },
-  }
+	static defaultProps = {
+		match: { params: {} },
+	}
 
-  static contextType = ThingContext
+	static contextType = ThingContext
 
-  componentDidMount() {
-    const { thingId } = this.props.match.params
-    this.context.clearError()
-    ThingApiService.getThing(thingId)
-      .then(this.context.setThing)
-      .catch(this.context.setError)
-    ThingApiService.getThingReviews(thingId)
-      .then(this.context.setReviews)
-      .catch(this.context.setError)
-  }
+	componentDidMount() {
+		const { thingId } = this.props.match.params
+		this.context.clearError()
+		ThingApiService.getThing(thingId)
+			.then(this.context.setThing)
+			.catch(this.context.setError)
+		ThingApiService.getThingReviews(thingId)
+			.then(this.context.setReviews)
+			.catch(this.context.setError)
+	}
 
-  componentWillUnmount() {
-    this.context.clearThing()
-  }
+	componentWillUnmount() {
+		this.context.clearThing()
+	}
 
-  renderThing() {
-    const { thing, reviews } = this.context
-    return <>
-      <div className='ThingPage__image' style={{backgroundImage: `url(${thing.image})`}} />
-      <h2>{thing.title}</h2>
-      <ThingContent thing={thing} />
-      <ThingReviews reviews={reviews} />
-      <ReviewForm />
-    </>
-  }
+	renderThing() {
+		const { thing, reviews } = this.context
+		return (
+			<>
+				<div
+					className='ThingPage__image'
+					style={{ backgroundImage: `url(${thing.image})` }}
+				/>
+				<h2>{thing.title}</h2>
+				<ThingContent thing={thing} />
+				<ThingReviews reviews={reviews} />
+				<ReviewForm />
+			</>
+		)
+	}
 
-  render() {
-    const { error, thing } = this.context
-    let content
-    if (error) {
-      content = (error.error === `Thing doesn't exist`)
-        ? <p className='red'>Thing not found</p>
-        : <p className='red'>There was an error</p>
-    } else if (!thing.id) {
-      content = <div className='loading' />
-    } else {
-      content = this.renderThing()
-    }
-    return (
-      <Section className='ThingPage'>
-        {content}
-      </Section>
-    )
-  }
+	render() {
+		const { error, thing } = this.context
+		let content
+		if (error) {
+			content =
+				error.error === `Thing doesn't exist` ? (
+					<p className='red'>Thing not found</p>
+				) : (
+					<p className='red'>{error.error}</p>
+				)
+		} else if (!thing.id) {
+			content = <div className='loading' />
+		} else {
+			content = this.renderThing()
+		}
+		return <Section className='ThingPage'>{content}</Section>
+	}
 }
 
 function ThingContent({ thing }) {
-  return (
-    <p className='ThingPage__content'>
-      {thing.content}
-    </p>
-  )
+	return <p className='ThingPage__content'>{thing.content}</p>
 }
 
 function ThingReviews({ reviews = [] }) {
-  return (
-    <ul className='ThingPage__review-list'>
-      {reviews.map(review =>
-        <li key={review.id} className='ThingPage__review'>
-          <p className='ThingPage__review-text'>
-            <FontAwesomeIcon
-              size='lg'
-              icon='quote-left'
-              className='ThingPage__review-icon blue'
-            />
-            {review.text}
-          </p>
-          <p className='ThingPage__review-user'>
-            <ThingStarRating rating={review.rating} />
-            <Hyph />
-            {review.user.full_name}
-          </p>
-        </li>
-      )}
-    </ul>
-  )
+	return (
+		<ul className='ThingPage__review-list'>
+			{reviews.map((review) => (
+				<li key={review.id} className='ThingPage__review'>
+					<p className='ThingPage__review-text'>
+						<FontAwesomeIcon
+							size='lg'
+							icon='quote-left'
+							className='ThingPage__review-icon blue'
+						/>
+						{review.text}
+					</p>
+					<p className='ThingPage__review-user'>
+						<ThingStarRating rating={review.rating} />
+						<Hyph />
+						{review.user.full_name}
+					</p>
+				</li>
+			))}
+		</ul>
+	)
 }
